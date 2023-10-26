50e6ac8eb7623ba85f2be3a31eb01f96c7a5b7c7
gtejakrishna2000 <85777606+gtejakrishna2000@users.noreply.github.com>
2023-10-26 23:36:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+   let fname = document.etElementById("form1").fname.value
+	let lname = document.etElementById("form1").lname.value
+	document.alert(fname+" "+lname);
 }
